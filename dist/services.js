81977497f3c2e0a15ff3b4fbe1cc203363d4aea2
Summer <Summerrichards01@gmail.com>
2019-02-06 13:28:24 -0600

    fixing footer using pull request

--- dist/services.js
+++ dist/services.js
@@ -171,7 +171,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../.npm/_npx/5293/lib/node_modules/parcel/src/builtins/bundle-url.js"}],"../../.npm/_npx/5293/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../../.npm/_npx/5293/lib/node_modules/parcel/src/builtins/bundle-url.js"}],"../../.npm/_npx/21304/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49493" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58022" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
@@ -340,4 +340,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},["../../.npm/_npx/5293/lib/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["../../.npm/_npx/21304/lib/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
