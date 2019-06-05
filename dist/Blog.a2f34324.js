c3a662c71815a17bdc08dcc500932a0c006c633e
Summer <Summerrichards01@gmail.com>
2019-06-04 21:04:22 -0500

    making changes to the appointment page by updating prices of phone repair

--- dist/Blog.a2f34324.js
+++ dist/Blog.a2f34324.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../.npm/_npx/4208/lib/node_modules/parcel/src/builtins/css-loader.js"}],"../../.npm/_npx/35240/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"_css_loader":"../../.npm/_npx/4208/lib/node_modules/parcel/src/builtins/css-loader.js"}],"../../.npm/_npx/40401/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64505" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59342" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -392,5 +392,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../.npm/_npx/35240/lib/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["../../.npm/_npx/40401/lib/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
 //# sourceMappingURL=/Blog.a2f34324.js.map
